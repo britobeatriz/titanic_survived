6b60ff000e1fa65442fe2465bffe3b025971f5d9
Beatriz Brito <beatriz.debritolima1998@gmail.com>
2024-01-24 15:30:02 -0300

    all

--- app.py
+++ app.py
@@ -1,40 +1,30 @@
+# libs
 import gradio as gr
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
+import joblib as jb
 
-
-data = pd.read_csv('train.csv')
-
-X = data[["Sex","Age","Pclass"]].copy()
-y = data["Survived"]
-
-X["Sex"] =X["Sex"].map({"male":0, "female":1})
-X = X.fillna(-1)
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42)
-
-mdl = DecisionTreeClassifier(random_state=0, min_samples_leaf=6)
-mdl.fit(X_train, y_train)
-
-p = mdl.predict(X_test)
 
 def predict(Sex, Age, Pclass):
-    valores = pd.DataFrame({
-        "Sex":[Sex],
-        "Age":[Age],
-        "Pclass":[Pclass]
-    })
-    if mdl.predict(valores) == [0]:
-        return "Não há chances de sobreviver"
+    # valores = pd.DataFrame({
+    #     "Sex":[Sex],
+    #     "Age":[Age],
+    #     "Pclass":[Pclass]
+    # })
+    mdl = jb.load('model.pk1')
+    
+    if Pclass == 'A':
+        Pclass = 1
+    elif Pclass == 'B':
+        Pclass = 2
     else: 
-        return "Há chances de sobreviver"
+        Pclass = 3
 
-listaDropdown = [1,2,3]
+    pred = mdl.predict_proba([[Sex,Age,Pclass]])[0]
+    return {"Did not survive:" + pred[0]+ "survived:" + pred[1]}
 
 demo = gr.Interface(
     fn=predict,
-    inputs=[gr.Number(value=0),gr.Number(value=0),gr.Dropdown(listaDropdown)],
-    outputs=[gr.Text]
+    inputs=[gr.Dropdown(['male','female'], type="index"),gr.Number(value=0),gr.Dropdown(['A','B','C'], type="index")],
+    outputs="label"
 )
+
+demo.launch()
